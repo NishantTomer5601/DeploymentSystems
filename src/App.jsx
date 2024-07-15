e3aacade928882a644560c24d29a2ad6753f2664
Nishant Tomer <nishanttomer5601@gmail.com>
2024-07-16 01:41:34 +0530

    Updated images

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import { useState } from 'react'
 
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     
@@ -55,7 +56,7 @@ function App() {
 
     
     <div className="md:block hidden w-1/2">
-      <img className="rounded-2xl" src="https://images.unsplash.com/photo-1616606103915-dea7be788566?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1887&q=80"/>
+      <img className="rounded-2xl" src="cricket_img.avif"/>
     </div>
   </div>
 </section>
